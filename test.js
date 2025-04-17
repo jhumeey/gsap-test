254f98172013a71379c4e585050a57bfe6616c5f
Aminat Yakubu <yakubuaminat94@gmail.com>
2025-04-18 00:39:18 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,10 @@
+<script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/gsap.min.js"></script>
+
+<script>
+ // This tween will rotate an element with a class of .my-element
+ gsap.to('.my-element', {
+  rotation: 360,
+  duration: 2,
+  ease: 'bounce.out'
+ })
+</script>
