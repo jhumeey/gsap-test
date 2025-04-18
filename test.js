6cc39a7c979ef54f7dd8d33ce0d54a6ebaa52957
Aminat Yakubu <yakubuaminat94@gmail.com>
2025-04-18 01:02:59 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,7 @@
-
-
-<script>
- // This tween will rotate an element with a class of .my-element
- gsap.to('.my-element', {
-  rotation: 360,
-  duration: 2,
-  ease: 'bounce.out'
- })
-</script>
+window.addEventListener("DOMContentLoaded", () => {
+    gsap.to('.hero_heading', {
+      rotation: 360,
+      duration: 2,
+      ease: 'bounce.out'
+    });
+  });
