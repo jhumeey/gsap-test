3861e63f8e224cbbbdf83469f380f20b42b1d758
Aminat Yakubu <yakubuaminat94@gmail.com>
2025-04-18 01:05:53 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,5 @@
-window.addEventListener("DOMContentLoaded", () => {
-    gsap.to('.hero_heading', {
+gsap.to('.hero_heading', {
       rotation: 360,
       duration: 2,
       ease: 'bounce.out'
     });
-  });
