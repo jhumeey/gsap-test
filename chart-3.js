a3df1e3602f0cbbb94c5328d76ab02355583f40f
Aminat Yakubu <yakubuaminat94@gmail.com>
2025-05-09 01:58:11 +0100

    Update chart-3.js

--- chart-3.js
+++ chart-3.js
@@ -1,6 +1,5 @@
-
+ gsap.registerPlugin(SplitText, ScrollTrigger);
 document.fonts.ready.then(() => {
-  gsap.registerPlugin(SplitText, ScrollTrigger);
   let text = document.querySelector(".footer_big-text");
 let cont = document.querySelector(".section-footer");
   gsap.set(text, { opacity: 1 });
@@ -14,7 +13,7 @@ let cont = document.querySelector(".section-footer");
 
   gsap.from(chars, {
     scrollTrigger: {
-      trigger: cont,
+      trigger: text,
       start: "top 90%", // when top of text hits 80% of viewport
       toggleActions: "play none none none", // or "play none none reverse" if you  want it to reverse
     },
