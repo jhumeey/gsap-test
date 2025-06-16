6742426bce9a1f0f847d8f4f5ac8c35047470b97
Aminat Yakubu <yakubuaminat94@gmail.com>
2025-06-16 15:25:31 +0100

    Create belred-swiper.js

---
+++ belred-swiper.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Swiper('.swiper', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop: false,
+      centeredSlides: true,
+    });
+  });
